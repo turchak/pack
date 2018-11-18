831961e72fc88ec54d86f5cc1dcd16ec7099c4e9
turchak <turchaksv@gmail.com>
2018-11-19 00:21:38 +0200

    delete styles

--- src/Components/Сalculator/index.js
+++ src/Components/Сalculator/index.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 class Calculator extends Component {
     render() {
         return (
-            <div style={{ width: `${100}px`}}>
+            <div>
                 Calculator
             </div>
         );
