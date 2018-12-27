5436020981f3b71bc7c2f99f4889a0646e25f299
turchak <turchaksv@gmail.com>
2018-12-27 18:33:32 +0200

    add json prices

--- src/Components/Calculator/index.js
+++ src/Components/Calculator/index.js
@@ -1,92 +1,63 @@
-// @flow
 import React from 'react';
+import * as Prices from '../../Storage/prices.json'
 
 
-type State = {
-        smallAmount: {
-            price: {
-                a5: number,
-                a4: number,
-                a3: number,
-                a3plus: number,
-                a2: number,
-            } | null,
-        },
-        mediumAmount: {
-            price: {
-                a5: number,
-                a4: number,
-                a3: number,
-                a3plus: number,
-                a2: number,
-            } | null,
-        },
-        largeAmount: {
-            price: {
-                a5: number,
-                a4: number,
-                a3: number,
-                a3plus: number,
-                a2: number,
-            } | null,
-        }
-    } 
 
-type Props = {};
+class Calculator extends React.Component {
 
-class Calculator extends React.Component<Props, State> {
-
-    state = {
-        smallAmount: null,
-        mediumAmount: null,
-        largeAmount: null,
+    state= {
+        a5: null,
+        a4: null,
+        a3: null,
+        a3plus: null,
+        a2: null,
     }
 
     componentDidMount() {
         //TODO: here request to tariff plan, but not its hardcore
+        const json = {
+            "a5": [ 1.05, 0.95, 0.9 ],
+            "a4": [ 1.4, 1.3, 1.2 ],
+            "a3": [ 2, 1.85, 1.7 ],
+            "a3plus": [ 2.5, 2.35, 2.2 ],
+            "a2": [ 3.75, 3.5, 3.25 ]
+        }
         this.setState({
-            smallAmount: {
-                price: {
-                    a5: 1.05,
-                    a4: 1.4,
-                    a3: 2,
-                    a3plus: 2.5,
-                    a2: 3.75
-                }
-            },
-            mediumAmount: {
-                price: {
-                    a5: 0.95,
-                    a4: 1.3,
-                    a3: 1.85,
-                    a3plus: 2.35,
-                    a2: 3.5,
-                }
-            },
-            largeAmount: {
-                price: {
-                    a5: 0.9,
-                    a4: 1.2,
-                    a3: 1.7,
-                    a3plus: 2.2,
-                    a2: 3.25,
-                }
-            }
+            a5: json.a5,
+            a4: json.a4,
+            a3: json.a3,
+            a3plus: json.a3plus,
+            a2: json.a2,
         })
+        
     }
 
-    render(): React.Node {
-        const { smallAmount, mediumAmount, largeAmount } = this.state;
-        if (smallAmount && mediumAmount && largeAmount) {
-            return (
-                <div>
-                    <ul>
-                        <li>A5: {smallAmount.price.a5}</li>
-                    </ul>
-                </div>
-            );
+    render() {
+        const { a5, a4, a3, a3plus, a2 } = this.state;
+        if ( !a5 || !a4 || !a3 || !a3plus || !a2 ) {
+            return null;
         }
-        return null
+        return (
+            <div>
+                <ul>
+                    {a5.map(val => {
+                        return <li key={val}>{val}</li>
+                    })}
+                    {a4.map(val => {
+                        return <li key={val}>{val}</li>
+                    })}
+                    {a3.map(val => {
+                        return <li key={val}>{val}</li>
+                    })}
+                    {a3plus.map(val => {
+                        return <li key={val}>{val}</li>
+                    })}
+                    {a2.map(val => {
+                        return <li key={val}>{val}</li>
+                    })}
+                </ul>
+            </div>
+        );
         
     }
 }
